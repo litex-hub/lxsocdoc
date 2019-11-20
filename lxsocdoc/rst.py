4b8e6f9a96ad74d68fdec3bef5be810a052490c1
Sean Cross <sean@xobs.io>
2019-11-21 06:32:43 +0800

    rst: properly detent hanging indents
    When the first line isn't indented the same as the rest,
    pad it out.  This solves this case:
    
          description="""this is a
                         description that
    		     would otherwise be
    		     dedended weirdly"""
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- lxsocdoc/rst.py
+++ lxsocdoc/rst.py
@@ -113,6 +113,28 @@ def print_table(table, stream):
         print("", file=stream)
     print("", file=stream)
 
+def pad_first_line_if_necessary(s):
+    if not isinstance(s, str):
+        return s
+    lines = s.split("\n")
+
+    # If there aren't at least two lines, don't do anything
+    if len(lines) < 2:
+        return s
+
+    # If the first line is blank, don't do anything
+    if lines[0].strip() == "":
+        return s
+
+    # If the pading on line 1 is greater than line 2, pad line 1
+    # and return the result
+    line_0_padding = len(lines[0]) - len(lines[0].lstrip(' '))
+    line_1_padding = len(lines[1]) - len(lines[1].lstrip(' '))
+    if (line_1_padding > 0) and (line_1_padding > line_0_padding):
+        lines[0] = " " * (line_1_padding - line_0_padding) + lines[0]
+        return "\n".join(lines)
+    return s
+
 def reflow(s, width=80):
     """Reflow the jagged text that gets generated as part
     of this Python comment.
@@ -126,7 +148,10 @@ def reflow(s, width=80):
 
     Finally, append it to a new string to be returned.
     """
+    if not isinstance(s, str):
+        return s
     out = []
+    s = pad_first_line_if_necessary(s)
     for piece in textwrap.dedent(s).split("\n\n"):
         trimmed_piece = textwrap.fill(textwrap.dedent(piece).strip(), width=width)
         out.append(trimmed_piece)
